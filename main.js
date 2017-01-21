bf5dce33dd2ab843f1189618b9efa4b1f1aa08cd
zoski <zoski@free.fr>
2017-01-21 17:22:28 +0100

    ,

--- main.js
+++ main.js
@@ -12,10 +12,10 @@ var msg = [
     /*To add a new message just copy this line:
 	{ message: 'Your message here', color: new RGB(0,255,0) },
 	You can get color codes here: http://www.colorpicker.com/ 	*/
-	{ message: "N'hésitez à rejoindre notre serveur Discord : http://raildecake.fr/discord", color: new RGB(255, 140, 0) },
-	{ message: "Un problème ? Des remarques laissez un message sur notre discord  http://raildecake.fr/discord" , color: new RGB(255, 140, 0) }
-	{ message: "Marre de te battre ? Utilise /passive pour retrouver la paix" , color: new RGB(255, 140, 0) }
-	{ message: "Besoin de vitesse ? Active le boost dans le menu d'achat" , color: new RGB(255, 140, 0) }
+	{ message: "N'hésitez à rejoindre notre serveur Discord : http://raildecake.fr/discord", color: new RGB(255,140,0) },
+	{ message: "Un problème ? Des remarques laissez un message sur notre discord  http://raildecake.fr/discord" , color: new RGB(255,140,0) },
+	{ message: "Marre de te battre ? Utilise /passive pour retrouver la paix" , color: new RGB(255,140,0) },
+	{ message: "Besoin de vitesse ? Active le boost dans le menu d'achat" , color: new RGB(255,140,0) }
 ];
 
 
