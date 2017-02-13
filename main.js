2cbd7cb44af1620e641fda5ba361193fdb031a22
Gaël A <zoski@free.fr>
2017-02-13 16:37:28 +0100

    Updating messages

--- main.js
+++ main.js
@@ -13,18 +13,22 @@ var msg = [
 	{ message: 'Your message here', color: new RGB(0,255,0) },
 	You can get color codes here: http://www.colorpicker.com/ 	*/
 	{ message: "N'hésitez à rejoindre notre serveur Discord : http://raildecake.fr/discord", color: new RGB(255,140,0) },
+  { message: "Even non French player are welcome. Please have a great time here ;-)", color: new RGB(255,140,0) },
 	{ message: "Un problème ? Des remarques laissez un message sur notre discord  http://raildecake.fr/discord" , color: new RGB(255,140,0) },
 	{ message: "Marre de te battre ? Utilise /passive pour retrouver la paix" , color: new RGB(255,140,0) },
-	{ message: "Besoin de vitesse ? Active le boost dans le menu d'achat" , color: new RGB(255,140,0) }
+	{ message: "Notre serveur redemarre seul automatiquement :D" , color: new RGB(255,140,0) },
+	{ message: "N'hésites à inviter tes potes, plus il y à de Rico plus il y à de Chaos !" , color: new RGB(255,140,0) },
+	{ message: "Besoin de vitesse ? Active le boost dans le menu d'achat (v pour ouvrir)" , color: new RGB(255,140,0) }
 ];
 
 
 //By default it'll pick a random one every 5 minutes!
-var minutes = 30
+var minutes = 7;
 var the_interval = minutes * 60 * 1000;
 setInterval(function() {
 	var rand = parseInt(Math.random()*msg.length);
 
 	chat.broadcast(msg[rand].message, msg[rand].color);
+	console.log(`[AP] ${msg[rand].message}`)
 
 }, the_interval);
